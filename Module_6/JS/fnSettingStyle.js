2feef3a2fe04d5dcc12f37f71db6c491f1226f19
Mando Gonzalvez <mandogonzalvez@Mandos-MacBook-Air.local>
2023-02-17 10:45:38 -0500

    add programming assignment 6, js folder, img folder, two html files

---
+++ Module_6/JS/fnSettingStyle.js
@@ -0,0 +1,27 @@
+function enlargeText() {
+    document.getElementById("p1").style.fontSize = "5em";
+    document.getElementById("p2").style.fontSize = "5em";
+    document.getElementById("p3").style.fontSize = "5em";
+    document.getElementById("p4").style.fontSize = "5em";
+    document.getElementById("p5").style.fontSize = "5em";
+}
+function shrinkText() {
+    document.getElementById("p1").style.fontSize = "1em";
+    document.getElementById("p2").style.fontSize = "1em";
+    document.getElementById("p3").style.fontSize = "1em";
+    document.getElementById("p4").style.fontSize = "1em";
+    document.getElementById("p5").style.fontSize = "1em";
+}
+
+function enlargeHeading1() {
+    document.getElementById("h1").style.fontSize = "5em";
+}
+function shrinkHeading1() {
+    document.getElementById("h1").style.fontSize = "1em";
+}
+function enlargeHeading2() {
+    document.getElementById("h2").style.fontSize = "5em";
+}
+function shrinkHeading2() {
+    document.getElementById("h2").style.fontSize = "1em";
+}
---
+++ Module_6/JS/hiding_objects.js
@@ -0,0 +1,30 @@
+function vanishMinutes(){
+    document.getElementById("8Minutes").style.visibility = "hidden";
+}
+function appearMinutes(){
+    document.getElementById("8Minutes").style.visibility = "visible";
+}
+function vanishGraduation(){
+    document.getElementById("Graduation").style.visibility = "hidden";
+}
+function appearGraduation(){
+    document.getElementById("Graduation").style.visibility = "visible";
+}
+function vanishPBCC(){
+    document.getElementById("PBCC").style.visibility = "hidden";
+}
+function appearPBCC(){
+    document.getElementById("PBCC").style.visibility = "visible";
+}
+function vanishPittsburgh(){
+    document.getElementById("Pittsburgh").style.visibility = "hidden";
+}
+function appearPittsburgh(){
+    document.getElementById("Pittsburgh").style.visibility = "visible";
+}
+function vanishSchedule(){
+    document.getElementById("Schedule").style.visibility = "hidden";
+}
+function appearSchedule(){
+    document.getElementById("Schedule").style.visibility = "visible";
+}
