8ac25228b15f71792a7ec523128b00135e96c996
Mando Gonzalvez <mandogonzalvez@Mandos-MacBook-Air.local>
2023-03-19 15:08:19 -0400

    add final touches to programming project 9

--- Module_9/Survey/form.js
+++ Module_9/Survey/form.js
@@ -1,34 +1,42 @@
-
-function checkField() {
-    if (document.getElementById("addressInput").value === "") {
-        document.getElementById("addressInput").style.backgroundColor = "#FFC0CB";
-       } else {
-        (document.getElementById("addressInput")).style.backgroundColor = "transparent";
-       } 
-}
-
-function stateField() {
-  
-}
-
 function checkForm() {
-    var allFields =[ 
-        document.getElementById("fName"),
-        document.getElementById("lName"),
-        document.getElementById("addressInput"),
-        document.getElementById("cityName"),
-        document.getElementById("stateName"),
-        document.getElementById("zip"),
-        document.getElementById("coffeePlace"),
-        document.getElementById("lunchSpot"),
-        document.getElementById("vacationName")
-    ];
-    for (var i = 0; i < allFields.length; i++) {
-        if (allFields[i].value.length === 0) {
-          alert("Please fill out all fields");
-          return false;
-        }
+  var allFields = [
+    document.getElementById("fName"),
+    document.getElementById("lName"),
+    document.getElementById("addressInput"),
+    document.getElementById("cityName"),
+    document.getElementById("stateName"),
+    document.getElementById("zip"),
+    document.getElementById("coffeePlace"),
+    document.getElementById("lunchSpot"),
+    document.getElementById("vacationName")
+  ];
+  
+  if (document.getElementById("addressInput").value === "") {
+    document.getElementById("addressInput").style.backgroundColor = "#FFC0CB";
+  }
+  if (document.getElementById("addressInput2").value === "") {
+    document.getElementById("addressInput2").style.backgroundColor = "#FFC0CB";
+  }
+  if (document.getElementById("coffeePlace").value === "") {
+    document.getElementById("coffeePlace").style.backgroundColor = "#FFFF00";
+  }
+  if (document.getElementById("lunchSpot").value === "") {
+    document.getElementById("lunchSpot").style.backgroundColor = "#FFFF00";
+  }
+  if (document.getElementById("vacationName").value === "") {
+    document.getElementById("vacationName").style.backgroundColor = "#FFFF00";
+  }
+  for (var i = 0; i < allFields.length; i++) {
+    if (allFields[i].value.length === 0) {
+      alert("Please fill out all fields");
+      return false;
     }
+  }
+  
+  return true;
+}
 
-      return true;
-    }
+function checkFieldColor(id) {
+  const input = document.getElementById(id);
+  input.style.backgroundColor = "";
+}
